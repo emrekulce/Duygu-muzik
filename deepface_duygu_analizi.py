0af8d7af82329682e1c0afb2753a7ecf2d96953b
emrekulce <78308609+emrekulce@users.noreply.github.com>
2023-08-19 05:34:32 +0300

    Update deepface_duygu_analizi.py

--- deepface_duygu_analizi.py
+++ deepface_duygu_analizi.py
@@ -23,13 +23,6 @@ def konusma(text):
     play(song)
 
 def foto_cek():
-    #camera = cv2.VideoCapture(0)# Video çekmeye başla
-    #return_value,image = camera.read()# İlk fotğrafı al
-    #cv2.imwrite('C:/Users/PC/Desktop/ben.jpg',image)#Kaydet
-    #camera.release()# ?
-    #cv2.destroyAllWindows()# Tüm ekranları kapat
-    #time.sleep(2)
-
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     cap = cv2.VideoCapture(0)
     cap.set(3,640) # set Width
@@ -38,24 +31,20 @@ def foto_cek():
     durum = False
     while True:
         ret, img = cap.read()
-        #img = cv2.rotate(img,cv2.ROTATE_180)  #görüntü ters çıkarsa bu komutla engelleyebilirsiniz.
-        img = cv2.flip(img, 1)  # veya burada 0-1-2 komutlarını deneyerek görüntünün döndüğünü görebilirsiniz.
+        img = cv2.flip(img, 1)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,minSize=(20, 20))
 
         for (x,y,w,h) in faces:
-            #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-            #roi_gray = gray[y:y+h, x:x+w]
-            #roi_color = img[y:y+h, x:x+w]
             print("Yüz Algılandı")
             time.sleep(5)
-            cv2.imwrite('ben.jpg',img)#Kaydet
+            cv2.imwrite('ben.jpg',img)
             durum = True
             break
 
         cv2.imshow('video',img)
         k = cv2.waitKey(30) & 0xff
-        if k == 27: # press 'ESC' to quit
+        if k == 27:
             break
         if durum == True:
             break
